bcfed4a37a8fc3d46f76f9267d6c940a7035e8ac
mirazizjon777 <mirazizjonisroilov1@gmail.com>
2023-03-25 07:12:17 +0500

    api add

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const darkText = document.querySelector(".dark-text");
 
 let b = false;
 darkNot.addEventListener("click", () => {
-    b++
+  b++;
   if (b == true) {
     wrapper.classList.toggle("dark-not");
     darkText.textContent = "LIGHT";
@@ -13,6 +13,40 @@ darkNot.addEventListener("click", () => {
     wrapper.classList.toggle("dark-not");
     darkText.textContent = "DARK";
     darkNot.src = "./images/svg/oy.svg";
-    b = false
+    b = false;
   }
 });
+
+//
+
+const search = document.getElementById("search");
+const input = document.querySelector("input");
+const nameEl = document.getElementById("name");
+const reposEl = document.getElementById("repos");
+const bioEl = document.getElementById("bio");
+const followersEl = document.getElementById("followers");
+const followingEl = document.getElementById("following");
+const gitBlogEl = document.getElementById("gitBlog");
+const locationEl = document.getElementById('location')
+search.addEventListener("click", (e) => {
+  e.preventDefault();
+  apiFunc();
+});
+
+// API
+
+async function apiFunc() {
+  let namee = "";
+  namee = input.value
+  const res = await fetch(`https://api.github.com/users/${namee}`);
+  const data = await res.json();
+  console.log(data);
+  nameEl.textContent = data.name;
+  reposEl.textContent = data.public_repos;
+  // bioEl.textContent = data.bio
+  followersEl.textContent = data.followers
+  followingEl.textContent = data.following
+  gitBlogEl.setAttribute('href', `${data.html_url}`)
+  locationEl.textContent = data.location
+}
+
